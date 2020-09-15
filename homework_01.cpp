eff735eca88ab7ff18e4e5777002567f6bbc5b8a
JJDOL <dadd55@naver.com>
2020-09-15 18:19:40 +0900

    HW01

---
+++ homework_01.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+//Å¬·¡½º´Â Ä¸½¶È­ 
+class SetValue {
+	int x, y;
+public :
+	void setX(int x);
+	void setY(int y);
+	int getX();
+	int getY();
+};
+
+void SetValue::setX(int n) {
+	x = n;
+}
+
+void SetValue::setY(int n) {
+	y = n;
+}
+
+int SetValue::getX() {
+	return x;
+}
+
+int SetValue::getY() {
+	return y;
+}
+
+int main() {
+	SetValue obj;
+	obj.setX(33);
+	obj.setY(44);
+
+	std::cout << "X = " << obj.getX() << " ,Y = " << obj.getY() << std::endl;
+	return 0;
+}
